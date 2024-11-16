8ea6835851e5ab66b6dd1a26de4f039264f9c70a
Rahul <venkatarahuly@gmail.com>
2024-11-16 00:29:02 +0000

    hrushi-mock

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,138 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+
+type ToDoItem = {
+  id: number;
+  task: string;
+  description: string;
+  status: boolean;
+};
+type ToDoList = ToDoItem[];
+
+const Page = () => {
+  const sampleData: ToDoList = [
+    {
+      id: 1,
+      task: "Do Laundry",
+      description: "maybe tonight or at tops tomorrow",
+      status: true,
+    },
+    {
+      id: 2,
+      task: "Buy Shampoo",
+      description: "on monday after the interview",
+      status: false,
+    },
+    {
+      id: 3,
+      task: "Prepare for interview",
+      description: "working on it now",
+      status: true,
+    },
+  ];
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [ifSearchHits, setIfSearchHits] = useState({
+    id: 0,
+    task: "NULL",
+    description: "NULL",
+    status: false,
+    checker: "false",
+  });
+  const [noSearchHit, setNoSearchHit] = useState(true);
+
+  const updateSearchQuery = (event: any) => {
+    // console.log(event.target.value);
+    setSearchQuery(event.target.value);
+  };
+
+  const searchHandler = (event: any) => {
+    event.preventDefault();
+
+    // check for the search query within all the todo items in the lsit we have
+    for (const element of sampleData) {
+      if (element.description.includes(searchQuery)) {
+        setIfSearchHits({
+          id: element.id,
+          task: element.task,
+          description: element.description,
+          status: element.status,
+          checker: "true",
+        });
+      }
+    }
+    // return back the particular todo element if there is a match
+    // otherwise just there is no match
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div>
+      <p className="text-4xl">ToDo:</p>
+      {sampleData.map((element: ToDoItem) => (
+        <ToDoItem
+          key={element.id}
+          task={element.task}
+          description={element.description}
+          status={element.status}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      ))}
+      <div className=" flex flex-col box-border h-32 w-80 border-2 border-black m-5 p-5">
+        <div className="flex flex-row">
+          <form onSubmit={searchHandler}>
+            <input
+              onChange={updateSearchQuery}
+              className="border-2"
+              value={searchQuery}
+            ></input>
+            <button className="border-2 border-black ml-2" type="submit">
+              Search
+            </button>
+          </form>
+          {ifSearchHits.checker === "true" && (
+            <ToDoItem
+              key={ifSearchHits.id}
+              task={ifSearchHits.task}
+              description={ifSearchHits.description}
+              status={ifSearchHits.status}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
-}
+};
+
+type ToDoItemProps = {
+  task: string;
+  description: string;
+  status: boolean;
+};
+
+const ToDoItem = ({ task, description, status }: ToDoItemProps) => {
+  return (
+    <div className=" flex flex-col box-border h-32 w-80 border-2 border-black m-5 p-5">
+      <div className="flex flex-row">
+        <p className="text-xl p-2">{task}</p>
+        {status ? (
+          <p className="text-l p-2 text-green-400">Status: done</p>
+        ) : (
+          <p className="text-l p-2 text-red-400">Status: not done</p>
+        )}
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Page;
+
+// ToDo: Notes - type definitions
+/*
+task, status -> 
+todoitem component: 
+Phase 1
+---
+Phase 2 
+
+*/
